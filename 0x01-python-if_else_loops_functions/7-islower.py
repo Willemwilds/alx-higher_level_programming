9c91dc567fba534e24af39b3c61f82dd1639c159
Wiliams <willemwilds@gmail.com>
2023-08-08 14:10:43 +0000

    if, else, loop and function task

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
 
-
-
-
-
-
-
-
-
-
-
-
-
 def islower(c):
-    if chr(c) not in range(65, 91)
+    if chr(c) not in range(65, 91):
         print("{} is lower".format(chr(c)))
     else:
         print("{} is upper".format(chr)c)))
