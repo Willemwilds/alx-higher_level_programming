d4b91029f6774ae055b2b08e0b076dfdf8612e11
Wiliams <willemwilds@gmail.com>
2023-08-08 16:02:48 +0000

    if, else, loop and function task

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for letter in str:
+        if ord(letter) not in range(65, 91):
+            letter = chr(ord(letter) - 32)
+        else:
+            letter = chr(ord(letter))
+        print(letter, end="")
+    print("\n")
