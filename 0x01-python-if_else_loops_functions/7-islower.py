edc70a4a699247148d4d026f4d8262b7424b4193
Wiliams <willemwilds@gmail.com>
2023-08-08 14:38:39 +0000

    if, else, loop and function task

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def islower(c):
-    if c.isalpha() and c.islower():
-        return True
-    else:
-        return False
+    if c.isalpha():
+        if c.islower():
+            return True
+        else:
+            return False
