6bf3838c5194d800ea64e2d61bb4e6810c37e0e2
Williams <willemwilds@gmail.com>
2023-08-10 22:30:05 +0100

    Import modules

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ = "main":
+    from calculator import add, sub, mul, div
+
+    a = 10
+    b = 5
+    
+    print("{:d} + {:d} = {:d}".format(a, b, add())
+    print("{:d} + {:d} = {:d}".format(a, b, sub())
+    print("{:d} + {:d} = {:d}".format(a, b, mul())
+    print("{:d} + {:d} = {:d}".format(a, b, div())
