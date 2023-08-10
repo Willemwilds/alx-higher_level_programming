c37c52c8bb070284ffd586d18382a890d75f6bc0
Williams <willemwilds@gmail.com>
2023-08-10 21:59:40 +0100

    Import modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/pyhton3
+if __name__ = "main":
+    from add_0 import add
 
-from add_0 import add
+    a = 1
+    b = 2
 
-a = 1
-b = 2
+    result = add(a, b)
 
-result = add(a, b)
-
-print("{:d} + {:d} = {:d}".format(a, b, result))
+    print("{:d} + {:d} = {:d}".format(a, b, result))
