8c53beba921c415f0e8ecf2a15482f3d0490be6f
Wiliams <willemwilds@gmail.com>
2023-08-11 11:54:06 +0000

    Command line arguments

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
 
-    if sys.argv != 4:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
@@ -16,9 +16,9 @@ if __name__ == "__main__":
     if operator == '+':
         result = add(a, b)
     elif operator == '-':
-        result == sub(a,b)
+        result = sub(a, b)
     elif operator == '*':
-        result == mul(a, b)
+        result = mul(a, b)
     elif operator == '/':
         result = div(a, b)
     else:
