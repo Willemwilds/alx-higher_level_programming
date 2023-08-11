194ac832daf1d8d13af458492b9b6dd3f892f36d
Wiliams <willemwilds@gmail.com>
2023-08-11 13:28:50 +0000

    Import module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
 
     import hidden_4
 
-    for name in hidden_4.names:
+    for name in dir(hidden_4):
         if not name.startswith('__'):
             print("{}".format(name))
