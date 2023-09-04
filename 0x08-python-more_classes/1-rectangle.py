4a1dc5888b651d733744df386fffb8a2baa7cdac
Wiliams <willemwilds@gmail.com>
2023-09-04 20:10:59 +0000

    Python classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,8 +18,8 @@ class Rectangle:
             ValueError: error if value less tjan zero.
         """
 
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
