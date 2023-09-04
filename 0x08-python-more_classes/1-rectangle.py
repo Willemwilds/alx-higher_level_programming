99704115b10b6be2c0f44ff8ea1c922322e1fe33
Wiliams <willemwilds@gmail.com>
2023-09-04 19:53:04 +0000

    Python classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -17,8 +17,8 @@ class Rectangle:
             TypeError: error if non integer is passed
             ValueError: error if value less tjan zero.
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
