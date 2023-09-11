58ebfdd1caa7619f8226b9296156ea6229c4e132
Williams <willemwilds@gmail.com>
2023-09-11 22:36:21 +0100

    Class inheritance

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module defines MyInt class.
+"""
+
+class MyInt(int):
+
+    """
+    Returns opposite values of the operators '==' and '!='
+    """
+    def __eq__(self, other):
+        return super().__ne__(other)
+    
+    def __ne__(self, other):
+        return super().__eq__(other)
