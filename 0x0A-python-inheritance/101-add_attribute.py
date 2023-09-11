5cc6b38de77fcb2412dd4caf8a9774058882532d
Williams <willemwilds@gmail.com>
2023-09-11 22:44:30 +0100

    Class inheritance

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""
+This module defines the function add_new_attribute.
+"""
 
 
 def add_new_attribute(obj, attr_name, attr_value):
+    """
+    This function adds new attribute to an object.
+    """
+
     if not hasattr(obj, '__dict__') and not hasattr(type(obj), '__slots__'):
         raise TypeError("can't add new attribute")
     setattr(obj, attr_name, attr_value)
