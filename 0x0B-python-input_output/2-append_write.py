0742b522da8f5d92aa90434dc8f22ff618db5280
Williams <willemwilds@gmail.com>
2023-09-12 12:42:57 +0100

    Python ifile input andd output

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""""
+This module defines a function that appends a string to an
+existing file.
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    This function returns the number of characters added to
+    the existing file.
+    """
+
+    with open(filename, 'a') as file:
+        return file.append(text)
