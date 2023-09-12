3c714643c2d249c839153f94f8ac84a5b9896228
Williams <willemwilds@gmail.com>
2023-09-12 12:19:13 +0100

    Python ifile input andd output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module writes a function that reads a text file
+"""
+
+
+def read_file(filename=""):
+    """
+    This function prints the content of the file  into
+    standard output.
+    """
+    
+    with open(filename) as f:
+        print(f.read(), end=" ")
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,1 @@
+This directory contains tasks involving python file input and output in python.
