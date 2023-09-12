e5e4149b84d53fa0f53f752ac880cdc5f61d8b13
Williams <willemwilds@gmail.com>
2023-09-12 13:26:32 +0100

    Python ifile input andd output

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -11,6 +11,6 @@ def load_from_json_file(filename):
     Returns a python object from the json string.
     """
 
-    with open(filename, 'w') as file:
-        python_file = json.load(file)
-        return python_file
+    with open(filename, 'r') as file:
+        python_data = json.load(file)
+        return python_data
