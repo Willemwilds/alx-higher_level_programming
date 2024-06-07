900daa311dc6d23af76dafb4c40e039f51ba4433
Williams <willemwilds@gmail.com>
2024-06-07 22:33:46 +0100

    sql queries

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,3 +1,6 @@
 --Creates the table "force_name' on the database " hbtn_0d_2"
 
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
